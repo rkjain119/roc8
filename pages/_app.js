56d23c3b132ac5b22362eea0fee17f7bc40e1503
rkjain119 <rkjain119@gmail.com>
2022-09-09 17:02:21 +0530

    📦 NEW: Progress loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 import { UserProvider } from '../context/users'
+import NextNProgress from 'nextjs-progressbar'
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <Layout>
+        <NextNProgress color="#fb7185" />
         <Component {...pageProps} />
       </Layout>
     </UserProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import { useEffect, useState } from "react";
-import Landing from "../components/landing";
-import Dashboard from "../components/dashboard";
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import { ChevronRightIcon } from "@heroicons/react/solid";
+import Landing from '../components/landing'
+import Dashboard from '../components/dashboard'
 
-import { isLoading, UserState } from "../context/users";
+import { UserState } from '../context/users'
 
-function Home() {
-	return UserState() ? <Dashboard /> : <Landing />;
+function Slash() {
+  return UserState() ? <Dashboard /> : <Landing />
 }
-export default Home;
+export default Slash
